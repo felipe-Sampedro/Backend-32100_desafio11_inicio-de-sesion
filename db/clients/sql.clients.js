42bff76906dbcc0c27cddae43fb38d7433b8a2af
felipe.sampedro <pipesancho182@gmail.com>
2022-12-15 00:10:23 -0500

    hice hasta donde pude

---
+++ db/clients/sql.clients.js
@@ -0,0 +1,80 @@
+const knex = require('knex')
+
+module.exports = class SQLClient {
+    constructor(config){
+        this.knex = knex(config);
+    }
+
+
+    creatTable(tableName){
+        return this.knex.schema.hasTable(tableName)
+            .then((response)=>{
+                if(!response){
+                   return this.knex.schema.createTable(tableName, (table)=>{
+                        table.increments('id').notNullable().primary();
+                        table.string('nombre').notNullable();                        
+                        table.float('precio');
+                        table.string('imagen');
+                    });
+                }            
+            }).catch((err) => console.log("error en constructor productos", err));    
+    }
+
+
+    createTableMessagges(tableName){
+        return this.knex.schema.hasTable(tableName)
+        .then((resp) => {
+            if(!resp){
+                return this.knex.schema.createTable(tableName,(table) =>{
+                    table.increments('id').notNullable().primary();
+                    // table.string('id').notNullable().primary();
+                    table.string('username').notNullable();
+                    table.string('time').notNullable();
+                    table.string('text');
+                });
+            }
+            console.log('the table was created');
+        }).catch((err) => console.log("error en constructor mensajes", err));    
+    }
+
+
+    insertRecords(tableName,items){
+        return this.knex(tableName).insert(items)
+        .then(()=>console.log('item inserted succesfully'));
+    }
+
+
+    getRecords(tableName){
+        return this.knex.from(tableName).select(
+            'id',
+            'nombre',
+            'precio',
+            'imagen',
+        );
+    }
+
+
+    deleteById(tableName, id){
+        return this.knex.from(tableName).where({id}).del()
+        .then(console.log('the item was deleted'));
+    }
+
+    updateRecordById(tableName, id,payload){
+        return this.knex.from(tableName).where({id}).update(payload)
+        .then(console.log('the item was updated'));
+    }
+
+
+    getMessages(tableName){
+        const messages = this.knex(tableName).select('id','username','time','text')
+        return messages
+        .catch((err) => console.log("error en constructor mensajes", err));  
+    }
+
+
+    disconnect(){
+        this.knex.destroy()
+    }
+
+}
+
---
+++ models/products.js
@@ -0,0 +1,74 @@
+const products = [
+    {
+      id: 1,
+      nombre: 'Escuadra',
+      precio: 323.45,
+      imagen: 'https://upload.wikimedia.org/wikipedia/commons/3/3c/Squadra_45.jpg'
+    },
+    {
+      id: 2,
+      nombre: 'Calculadora',
+      precio: 234.56,
+      imagen: 'https://exitocol.vtexassets.com/arquivos/ids/257195/Calculadora-Cientifica-Graficadora-Texas-Ti-nspire-cxcas-3d.jpg?v=637002371419830000'
+    }
+  ]
+  
+  class Products {
+    static lastProductId = products[products.length - 1].id;
+    
+    constructor() {
+      this.list = products;
+    }
+  
+    getAll() {
+      return this.list;
+    }
+  
+    getById(productId) {
+      return this.list.find(product => product.id === +productId);
+    }
+  
+    save(product) {
+      const { nombre, precio, imagen } = product;
+      if ( !nombre || !precio || !imagen) {
+        return null;
+      }
+      Products.lastProductId++;
+      const newProduct = {
+        id: Products.lastProductId,
+        nombre,
+        precio,
+        imagen
+      };
+      console.log(newProduct);
+      this.list.push(newProduct);
+      return newProduct;
+    };
+
+
+    updateById(productId, product) {
+      const productIndex = this.list.findIndex((producto) => producto.id === +productId);
+      if (productIndex < 0) return null;
+      const {
+        nombre,
+        precio,
+        imagen
+      } = product;
+      const updatedProduct = {
+        id: this.list[productIndex].id,
+        nombre,
+        precio,
+        imagen
+      };
+      this.list[productIndex] = updatedProduct;
+      return updatedProduct;
+    }
+  
+    deleteById(productId) {
+      const productIndex = this.list.findIndex((producto) => producto.id === +productId);
+      if (productIndex < 0) return null;
+      return this.list.splice(productIndex, 1);
+    }
+  }
+  
+  module.exports = Products;
